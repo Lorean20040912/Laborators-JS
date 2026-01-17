3ab9ad258bdfddbc760f3374642e2cc3ced2908a
OCR_INVOICE <loreanmandrescu2004@gmail.com>
2026-01-17 11:18:01 +0200

    try to rezolv ex1

---
+++ .idea/app.js
@@ -0,0 +1,19 @@
+const dateofbirth=21;
+const name="Lorean";
+let studying=true;
+
+
+
+console.log("Hello from app.js");
+let x = 2;
+let y = 3;
+
+console.log('x=', x);
+console.log('y=', y);
+
+let r = 24;
+const pi = 3.14;
+
+console.log('r =', r);
+console.log('pi =', pi);
+
